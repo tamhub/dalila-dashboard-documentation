c32350c264c39926f873f1d60d341a14740fa490
Weam Adel <weam.adel@robustastudio.com>
2025-05-24 09:59:50 +0300

    chore: fix deployment configurations

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,16 @@ const config: Config = {
 	favicon: 'img/favicon.ico',
 
 	// Set the production url of your site here
-	url: 'https://your-docusaurus-site.example.com',
+	url: 'https://tamhub.github.io',
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: '/',
+	baseUrl: '/dalila-dashboard-documentation/',
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
 	organizationName: 'tamhub', // Usually your GitHub org/user name.
 	projectName: 'dalila-dashboard-documentation', // Usually your repo name.
+	trailingSlash: false,
 
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
